dde7cbf3495864c919777f00d5defab6d8203422
rasecsyn <cesar_synyster@hotmail.com>
2016-04-05 02:01:39 -0300

    Adicionado Ex2
    Em andamento

---
+++ respostas/bhwe_03_cesaraugustosantosdasilva/ex03.php
@@ -0,0 +1,33 @@
+<!DOCTYPE 	HTML>
+<html>
+
+<head>
+	<title>Ex03</title>
+	<meta charset="UTF-8">
+</head>
+
+<body>
+
+
+<h1>Ex03</h1>
+
+
+
+
+
+
+<p>Crie uma arquitetura MVC utilizando a linguagem PHP, este sistema não precisa ter uma funcionalidade, mas tem de ter as seguintes funções:
+	OOP (Programação Orientado a Objetos).
+    CRUD
+    Conexão com DB (PDO ou MySQLi).
+    Upload de arquivos (um para imagens e outro para arquivos).
+	Obs: não se esqueça de enviar o arquivo .sql junto para que possa ser feito os testes.
+</p>
+
+
+
+
+
+
+
+</body>
